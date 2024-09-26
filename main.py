dfc4366555eb2fa7993bdd846aa3f1bda1e37d7f
zzyo77 <51728687+zzyo77@users.noreply.github.com>
2024-09-26 23:05:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ def login(user, password):
 def main(_user, _passwd, min_1, max_1):
     user = str(_user)
     password = str(_passwd)
-    step = str(random.randint(min_1, max_1))
+    step = str(random.randint(19998, 19999))
     print("已设置为随机步数(" + str(min_1) + "~" + str(max_1) + ")")
     if user == '' or password == '':
         print("用户名或密码填写有误！")
